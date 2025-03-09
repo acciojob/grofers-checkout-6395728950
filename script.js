7df920132c1a1a3211a695c1095794d424014267
6395728950 <161918761+6395728950@users.noreply.github.com>
2025-03-09 09:59:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,23 @@
 const getSumBtn = document.createElement("button");
 getSumBtn.append("Get Total Price");
-document.body.appendChild(getSumBtn);
+const newrow = document.createElement("tr");
+const table = document.querySelector("table");
+ newrow.appendChild(getSumBtn);
+ 
+// document.body.appendChild(getSumBtn);
 
 const getSum = () => {
-//Add your code here
+ let sum =0;
+	 document.querySelectorAll(".price").forEach((val)=>{
+	  sum+=parseFloat(val.textContent) || 0;
+ });
+	const totalsum =document.createElement("td");
+	totalsum.textContent = sum;
+	 
+	// document.body.appendChild(totalsum);
+	newrow.appendChild(totalsum);
   
 };
 
 getSumBtn.addEventListener("click", getSum);
-
+table.appendChild(newrow);
